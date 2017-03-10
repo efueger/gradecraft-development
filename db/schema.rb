bba1375f7550f037ea9dc0d3510b982bc14157db
Jonathan Gabel <hello@jonathangabel.com>
2017-03-10 13:33:33 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309154739) do
+ActiveRecord::Schema.define(version: 20170310181729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -371,6 +371,8 @@ ActiveRecord::Schema.define(version: 20170309154739) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "store_dir"
+    t.integer  "course_id"
+    t.integer  "assignment_id"
   end
 
   create_table "flagged_users", force: :cascade do |t|
